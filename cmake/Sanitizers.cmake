f28a72ce9808b1a96746a92480a78f3fa6759be2
Pavel Cherniavskyi <PCherniavskyi@luxoft.com>
2020-05-27 17:47:18 +0300

    All cmake configuration almost done

--- cmake/Sanitizers.cmake
+++ cmake/Sanitizers.cmake
@@ -2,7 +2,7 @@ function(enable_sanitizers project_name)
 
     if(CMAKE_CXX_COMPILER_ID STREQUAL "GNU" OR CMAKE_CXX_COMPILER_ID MATCHES ".*Clang")
 
-        option(ENABLE_COVERAGE "Enable coverage reporting for gcc/clang" ON)
+        option(ENABLE_COVERAGE "Enable coverage reporting for gcc/clang" OFF)
         if(ENABLE_COVERAGE)
             target_compile_options(${project_name} INTERFACE --coverage -O0 -g)
             target_link_libraries(${project_name} INTERFACE --coverage)
@@ -10,24 +10,19 @@ function(enable_sanitizers project_name)
 
         set(SANITIZERS "")
 
-        option(ENABLE_SANITIZER_ADDRESS "Enable address sanitizer" ON)
+        option(ENABLE_SANITIZER_ADDRESS "Enable address sanitizer" OFF)
         if(ENABLE_SANITIZER_ADDRESS)
             list(APPEND SANITIZERS "address")
         endif()
 
-        option(ENABLE_SANITIZER_LEAK "Enable leak memory sanitizer" ON)
+        option(ENABLE_SANITIZER_LEAK "Enable leak memory sanitizer" OFF)
         if(ENABLE_SANITIZER_LEAK)
             list(APPEND SANITIZERS "leak")
         endif()
 
-        option(ENABLE_SANITIZER_UNDEFINED_BEHAVIOR "Enable undefined behavior sanitizer" ON)
+        option(ENABLE_SANITIZER_UNDEFINED_BEHAVIOR "Enable undefined behavior sanitizer" OFF)
         if(ENABLE_SANITIZER_UNDEFINED_BEHAVIOR)
             list(APPEND SANITIZERS "undefined")
-        endif()
-
-        option(ENABLE_SANITIZER_THREAD "Enable thread sanitizer" OFF)
-        if(ENABLE_SANITIZER_THREAD)
-            list(APPEND SANITIZERS "thread")
         endif()
 
         list(JOIN SANITIZERS "," LIST_OF_SANITIZERS)
