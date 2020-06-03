2c38264ea4f13b2a041762100255d59e4d942319
Pavel Cherniavskyi <PCherniavskyi@luxoft.com>
2020-06-03 19:14:48 +0300

    test for SpriteHolder is done

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,14 +1,10 @@
 #include <catch2/catch.hpp>
+#include "SpriteHolder.hpp"
 
-unsigned int Factorial(unsigned int number)
+TEST_CASE("SpriteHolder functionality", "[SpriteHolder]")
 {
-   return number <= 1 ? number : Factorial(number - 1) * number;
-}
-
-TEST_CASE("Factorials are computed", "[factorial]")
-{
-   REQUIRE(Factorial(1) == 1);
-   REQUIRE(Factorial(2) == 2);
-   REQUIRE(Factorial(3) == 6);
-   REQUIRE(Factorial(10) == 3628800);
+   SpriteHolder spriteHolder;
+   spriteHolder.init();
+   sf::Sprite sprite = spriteHolder.getSprite(SpriteId::MAIN_TANK1);
+   REQUIRE(sprite.getTextureRect() != sf::IntRect());
 }
