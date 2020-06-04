c3d7be003612ddc63f4c3bce02c4f9b74a1eeed6
Pavel Cherniavskyi <PCherniavskyi@luxoft.com>
2020-06-04 16:05:58 +0300

    ResourceHolder is added

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,5 +1,14 @@
 #include <catch2/catch.hpp>
+#include <config.h>
 #include "SpriteHolder.hpp"
+
+TEST_CASE("ResourceHolder functionality", "[ResourceHolder]")
+{
+   ResourceHolder<Textures, sf::Texture> resourceHolder;
+   resourceHolder.load(Textures::MAIN_SHEET, fmt::format("{}/media/Textures/SpriteSheet.png", CMAKE_SOURCE_DIR));
+   const sf::Texture& texture = resourceHolder.get(Textures::MAIN_SHEET);
+   REQUIRE(texture.getSize() != sf::Vector2u());
+}
 
 TEST_CASE("SpriteHolder functionality", "[SpriteHolder]")
 {
