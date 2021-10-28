2026a960d9cd6b9b8ee027ff22948a37d6b20093
Pavel Cherniavskyi <cherniavskyi@gmail.com>
2021-10-28 16:44:39 +0300

    ResourceHolder templates improvement

--- src/ResourceHolders/ResourceHolder.hpp
+++ src/ResourceHolders/ResourceHolder.hpp
@@ -6,12 +6,13 @@
 #define BATTLECITY_RESOURCEHOLDER_HPP
 
 #include <SFML/Graphics/Rect.hpp>
+#include "../Utils/Utils.hpp"
 
 template<typename Identifier, typename Resource>
 class ResourceHolder
 {
 public:
-  void LoadFromFile(Identifier, const std::string&);
+  void LoadFromFile(Identifier, const std::string&, const std::vector<sf::IntRect>& aRec = {});
   void Insert(Identifier, const Resource&);
   std::shared_ptr<Resource> Get(Identifier) const;
 
@@ -19,51 +20,32 @@ private:
   std::unordered_map<Identifier, std::shared_ptr<Resource>> mResourceMap;
 };
 
-template<typename Identifier, typename T>
-class ResourceHolder<Identifier, std::vector<T>>
-{
-public:
-  void LoadFromFile(Identifier, const std::string&, const std::vector<sf::IntRect>&);
-  void Insert(Identifier, const std::vector<T>&);
-  std::shared_ptr<std::vector<T>> Get(Identifier) const;
-
-private:
-  std::unordered_map<Identifier, std::shared_ptr<std::vector<T>>> mResourceMap;
-};
-
-
 template<typename Identifier, typename Resource>
-void ResourceHolder<Identifier, Resource>::LoadFromFile(Identifier aId, const std::string& aFilename)
+void ResourceHolder<Identifier, Resource>::LoadFromFile(Identifier aId, const std::string& aFilename, const std::vector<sf::IntRect>& aRec)
 {
   if (mResourceMap.find(aId) == mResourceMap.end())
   {
-    Resource resource;
-    if (resource.loadFromFile(aFilename))
+    if constexpr(Utils::is_vector<Resource>::value)
     {
-      Insert(aId, resource);
+      for(auto i = 0u; i < aRec.size(); ++i)
+      {
+        typename Resource::value_type resource;
+        if (resource.loadFromFile(aFilename, aRec[i]))
+        {
+          Insert(aId, {resource});
+        }
+        else
+        {
+          SPDLOG_ERROR("Failed to load from file");
+        }
+      }
     }
     else
     {
-      SPDLOG_ERROR("Failed to load from file");
-    }
-  }
-  else
-  {
-    SPDLOG_WARN("Resource: {} is already loaded", aId._to_string());
-  }
-}
-
-template<typename Identifier, typename T>
-void ResourceHolder<Identifier, std::vector<T>>::LoadFromFile(Identifier aId, const std::string& aFilename, const std::vector<sf::IntRect>& aRec)
-{
-  if (mResourceMap.find(aId) == mResourceMap.end())
-  {
-    for(auto i = 0u; i < aRec.size(); ++i)
-    {
-      T resource;
-      if (resource.loadFromFile(aFilename, aRec[i]))
+      Resource resource;
+      if (resource.loadFromFile(aFilename))
       {
-        Insert(aId, {resource});
+        Insert(aId, resource);
       }
       else
       {
@@ -80,41 +62,27 @@ void ResourceHolder<Identifier, std::vector<T>>::LoadFromFile(Identifier aId, co
 template<typename Identifier, typename Resource>
 void ResourceHolder<Identifier, Resource>::Insert(Identifier aId, const Resource& aRes)
 {
-  mResourceMap.insert(std::make_pair(aId, std::make_shared<Resource>(aRes)));
-}
-
-template<typename Identifier, typename T>
-void ResourceHolder<Identifier, std::vector<T>>::Insert(Identifier aId, const std::vector<T>& aRes)
-{
-  auto it = mResourceMap.find(aId);
-  if (it != mResourceMap.end() && it->second)
+  if constexpr(Utils::is_vector<Resource>::value)
   {
-    it->second->insert(it->second->end(), aRes.begin(), aRes.end());
+    auto it = mResourceMap.find(aId);
+    if (it != mResourceMap.end() && it->second)
+    {
+      it->second->insert(it->second->end(), aRes.begin(), aRes.end());
+    }
+    else
+    {
+      mResourceMap.insert(std::make_pair(aId, std::make_shared<std::vector<typename Resource::value_type>>(aRes)));
+    }
   }
   else
   {
-    mResourceMap.insert(std::make_pair(aId, std::make_shared<std::vector<T>>(aRes)));
+    mResourceMap.insert(std::make_pair(aId, std::make_shared<Resource>(aRes)));
   }
   
 }
 
 template<typename Identifier, typename Resource>
 std::shared_ptr<Resource> ResourceHolder<Identifier, Resource>::Get(Identifier aId) const
-{
-  auto it = mResourceMap.find(aId);
-  if (it == mResourceMap.end())
-  {
-    SPDLOG_ERROR("Resource doesn't exist");
-    return nullptr;
-  }
-  else
-  {
-    return it->second;
-  }
-}
-
-template<typename Identifier, typename T>
-std::shared_ptr<std::vector<T>> ResourceHolder<Identifier, std::vector<T>>::Get(Identifier aId) const
 {
   auto it = mResourceMap.find(aId);
   if (it == mResourceMap.end())
--- src/Utils/Utils.hpp
+++ src/Utils/Utils.hpp
@@ -2,11 +2,23 @@
 #define BATTLECITY_UTILS_UTILS_HPP
 
 #include "../Enums.hpp"
+#include <type_traits>
 
 namespace Utils{
 
 
 bool IsMovingAction(EActions action);
+
+template <typename T, typename _ = void>
+struct is_vector { 
+    static const bool value = false;
+};
+
+template <typename T>
+struct is_vector< T, typename std::enable_if_t<std::is_same_v<T, std::vector<typename T::value_type>>>>
+{
+    static const bool value = true;
+};
 
 
 }
