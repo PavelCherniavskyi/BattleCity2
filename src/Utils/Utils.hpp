3e0800ab6a8bc222f6a320118f602a4240a833d0
Pavel Cherniavskyi <cherniavskyi@gmail.com>
2021-10-29 12:40:45 +0300

    Make Utils header only library with C++17 inline

--- src/Utils/Utils.hpp
+++ src/Utils/Utils.hpp
@@ -7,7 +7,20 @@
 namespace Utils{
 
 
-bool IsMovingAction(EActions action);
+inline bool IsMovingAction(EActions action)
+{
+  switch (action)
+  {
+  case +EActions::LEFT:
+  case +EActions::RIGHT:
+  case +EActions::UP:
+  case +EActions::DOWN: 
+    return true;
+
+  default:
+    return false;
+  }
+}
 
 template <typename T, typename _ = void>
 struct is_vector { 
